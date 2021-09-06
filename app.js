a3d582543dc24aa2d0027c5529e7b2af081e80ec
Arzhan Bakytbek <79097350+roses-are-dead666@users.noreply.github.com>
2021-09-06 17:14:46 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+$(document).on('scroll', function(){
+    if ( $(window).scrollTop() > 30) {
+        $('#header').addClass('changeColor');
+    } else {
+        $('#header').removeClass('changeColor');
+    }
+});
